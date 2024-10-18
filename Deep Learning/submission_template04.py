48b28a132a6c49767b4bca3942f6e22eeb51e645
colaaao6 <1242927286@qq.com>
2024-10-19 02:38:16 +0800

    Update submission_template04.py

--- Deep Learning/submission_template04.py
+++ Deep Learning/submission_template04.py
@@ -1,15 +1,20 @@
-import numpy as np
-import torch
-from torch import nn
-from torch.nn import functional as F
+# conv1
+assert model.conv1.kernel_size == (5, 5), "неверный размер ядра у conv1"
+assert model.conv1.in_channels == 3, "неверный размер in_channels у conv1"
+assert model.conv1.out_channels == 3, "неверный размер out_channels у conv1"
 
-class ConvNet(nn.Module):
-    def __init__(self):
-        ...
+# pool1
+assert model.pool1.kernel_size == 2 or model.pool1.kernel_size == (2, 2), "неверный размер ядра у pool1"
 
-    
-    def forward(self, x):
-        ...
+# conv2
+assert model.conv2.kernel_size == (3, 3), "неверный размер ядра у conv2"
+assert model.conv2.in_channels == 3, "неверный размер in_channels у conv2"
+assert model.conv2.out_channels == 5, "неверный размер out_channels у conv2"
 
-def create_model():
-    return ConvNet()
+# pool2
+assert model.pool2.kernel_size == 2 or model.pool2.kernel_size == (2, 2), "неверный размер ядра у pool2"
+
+# fc1
+assert model.fc1.out_features == 100, "неверный размер out_features у fc1"
+# fc2
+assert model.fc2.out_features == 10, "неверный размер out_features у fc2"

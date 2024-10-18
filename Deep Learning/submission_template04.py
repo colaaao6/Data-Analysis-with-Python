4eb9b059545b6715bebd88995c78994893c5e91f
colaaao6 <1242927286@qq.com>
2024-10-19 02:37:34 +0800

    Update submission_template04.py

--- Deep Learning/submission_template04.py
+++ Deep Learning/submission_template04.py
@@ -1,15 +1,11 @@
 import numpy as np
+
 import torch
 from torch import nn
 from torch.nn import functional as F
 
-class ConvNet(nn.Module):
-    def __init__(self):
-        ...
+import torchvision
+from torchvision import datasets, transforms
 
-    
-    def forward(self, x):
-        ...
-
-def create_model():
-    return ConvNet()
+from matplotlib import pyplot as plt
+from IPython.display import clear_output

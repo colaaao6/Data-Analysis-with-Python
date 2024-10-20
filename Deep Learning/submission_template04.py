bf2c4b97af4bbd12fc01169aa360a16618227e2a
colaaao6 <1242927286@qq.com>
2024-10-20 20:53:23 +0800

    Update submission_template04.py

--- Deep Learning/submission_template04.py
+++ Deep Learning/submission_template04.py
@@ -1,15 +1,26 @@
-import numpy as np
-import torch
-from torch import nn
-from torch.nn import functional as F
+# Создание экземпляра модели
+model = ConvNet()
 
-class ConvNet(nn.Module):
-    def __init__(self):
-        ...
+# Проверка параметров conv1
+assert model.conv1.kernel_size == (5, 5), "Неверный размер ядра у conv1"
+assert model.conv1.in_channels == 3, "Неверный размер in_channels у conv1"
+assert model.conv1.out_channels == 3, "Неверный размер out_channels у conv1"
 
-    
-    def forward(self, x):
-        ...
+# Проверка параметров pool1
+assert model.pool1.kernel_size == 2 or model.pool1.kernel_size == (2, 2), "Неверный размер ядра у pool1"
 
-def create_model():
-    return ConvNet()
+# Проверка параметров conv2
+assert model.conv2.kernel_size == (3, 3), "Неверный размер ядра у conv2"
+assert model.conv2.in_channels == 3, "Неверный размер in_channels у conv2"
+assert model.conv2.out_channels == 5, "Неверный размер out_channels у conv2"
+
+# Проверка параметров pool2
+assert model.pool2.kernel_size == 2 or model.pool2.kernel_size == (2, 2), "Неверный размер ядра у pool2"
+
+# Проверка параметров fc1
+assert model.fc1.out_features == 100, "Неверный размер out_features у fc1"
+
+# Проверка параметров fc2
+assert model.fc2.out_features == 10, "Неверный размер out_features у fc2"
+
+print("Все проверки пройдены успешно!")
